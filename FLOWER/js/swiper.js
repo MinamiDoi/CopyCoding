db8ce26683f3235057c04ebc4a715186ddd5dc39
Minami <haveaniceday.mnm@gmail.com>
2024-04-14 21:55:14 +0900

    bouquetセクションのswiperの実装完了

---
+++ FLOWER/js/swiper.js
@@ -0,0 +1,9 @@
+const swiperBouquet = new Swiper('.bouquet-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    effect: 'slide',
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 20,
+    // speed: 500,
+});
